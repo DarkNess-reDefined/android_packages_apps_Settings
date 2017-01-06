ResurrectionRemix/Resurrection_packages_apps_Settings

Live Volume Steps [2/2]

src/com/android/settings/rr/animation/AnimationSettings.java
@@ -1,4 +1,4 @@
-package com.mrapocalypse.screwdshop.frags;
+package com.android.settings.rr.animation;
 
 import android.app.Activity;
 import android.content.Context;

DarkNess-reDefined/android_packages_apps_Settings

src/com/android/settings/dnd/animation/AnimationSettings.java
@@ -1,4 +1,4 @@
-package com.mrapocalypse.screwdshop.frags;
+package com.android.settings.dnd.animation;
 
 import android.app.Activity;
 import android.content.Context;