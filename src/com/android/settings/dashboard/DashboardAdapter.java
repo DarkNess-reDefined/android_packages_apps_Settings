3a9566a308c55609ac74878f09e73d2bfbc8e9a6
GreenArrow9 <catbat.pc@gmail.com>
2017-01-09 05:40:00 +0000

    Revert "Remove dashboard tile summaries [1/2]"
    This reverts commit 5ed4c3d8a3ce008e6b12b9cc7830d540942f6788.

--- src/com/android/settings/dashboard/DashboardAdapter.java
+++ src/com/android/settings/dashboard/DashboardAdapter.java
@@ -350,13 +350,8 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
         holder.icon.setImageDrawable(mCache.getIcon(tile.icon));
         holder.title.setText(tile.title);
         if (!TextUtils.isEmpty(tile.summary)) {
-           if ((Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.REMOVE_TILE_SUMMARY, 0) == 1)) {
-                holder.summary.setVisibility(View.GONE);
-            } else {
-                holder.summary.setText(tile.summary);
-                holder.summary.setVisibility(View.VISIBLE);
-            }
+            holder.summary.setText(tile.summary);
+            holder.summary.setVisibility(View.VISIBLE);
         } else {
             holder.summary.setVisibility(View.GONE);
         }
