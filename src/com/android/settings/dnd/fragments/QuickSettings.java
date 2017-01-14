1531ca518811dd776b16039cfd411bd59e306107
GreenArrow9 <catbat.pc@gmail.com>
2017-01-14 08:57:02 +0000

    reDefined: Introduce QsPanel in QuickSettings

--- src/com/android/settings/dnd/fragments/QuickSettings.java
+++ src/com/android/settings/dnd/fragments/QuickSettings.java
@@ -1,5 +1,13 @@
 package com.android.settings.dnd.fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.content.ContentResolver;
 import android.content.res.Resources;
@@ -11,26 +19,35 @@ import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.View;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.android.settings.R;
-import com.crdroid.settings.preferences.SeekBarPreference;
+import com.android.settings.dnd.Preferences.SeekBarPreference;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+import cyanogenmod.preference.CMSystemSettingListPreference;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 public class QuickSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
+    private static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
     private static final String PREF_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
     private static final String PREF_ROWS_PORTRAIT = "qs_rows_portrait";
     private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
     private static final String PREF_COLUMNS = "qs_columns";
 
+    private CMSystemSettingListPreference mQuickPulldown;
     private ListPreference mSysuiQqsCount;
     private ListPreference mRowsPortrait;
     private ListPreference mRowsLandscape;
     private ListPreference mQsColumns;
+
 
     @Override
     protected int getMetricsCategory() {
@@ -43,8 +60,9 @@ public class QuickSettings extends SettingsPreferenceFragment
 
         addPreferencesFromResource(R.xml.dnd_qssettings);
 
-        ContentResolver resolver = getActivity().getContentResolver();
+        final ContentResolver resolver = getActivity().getContentResolver();
 
+        mQuickPulldown = (CMSystemSettingListPreference) findPreference(STATUS_BAR_QUICK_QS_PULLDOWN);
         mSysuiQqsCount = (ListPreference) findPreference(PREF_SYSUI_QQS_COUNT);
         int SysuiQqsCount = Settings.Secure.getInt(resolver,
                Settings.Secure.QQS_COUNT, 5);
@@ -75,6 +93,21 @@ public class QuickSettings extends SettingsPreferenceFragment
         mQsColumns.setOnPreferenceChangeListener(this);
      }
 
+    private void updatePulldownSummary(int value) {
+        Resources res = getResources();
+        updatePulldownSummary(mQuickPulldown.getIntValue(0));
+
+        if (value == 0) {
+
+            // quick pulldown deactivated
+            mQuickPulldown.setSummary(res.getString(R.string.status_bar_quick_qs_pulldown_off));
+        } else {
+            String direction = res.getString(value == 2
+                    ? R.string.status_bar_quick_qs_pulldown_summary_left
+                    : R.string.status_bar_quick_qs_pulldown_summary_right);
+            mQuickPulldown.setSummary(res.getString(R.string.status_bar_quick_qs_pulldown_summary, direction));
+  }
+}
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
@@ -111,5 +144,5 @@ public class QuickSettings extends SettingsPreferenceFragment
             return true;
         }
         return false;
-    }
+  }
 }
