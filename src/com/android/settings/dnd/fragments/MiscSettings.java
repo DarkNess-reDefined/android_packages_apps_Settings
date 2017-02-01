06fc20ad834c22008e6995c3f0ee5eccc4cc9326
AKabhishek <abhishekkaushik.star@gmail.com>
2017-02-01 13:57:10 +0000

    Move Fingerprint authentication vibration to Ui
    - Clean useless mismerged stuff
    
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- src/com/android/settings/dnd/fragments/MiscSettings.java
+++ src/com/android/settings/dnd/fragments/MiscSettings.java
@@ -12,7 +12,6 @@ import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 
-import android.hardware.fingerprint.FingerprintManager;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.DevelopmentSettings;
 import com.android.settings.R;
@@ -25,8 +24,6 @@ public class MiscSettings extends SettingsPreferenceFragment
 
     private ListPreference mScreenshotType;
 
-        private FingerprintManager mFingerprintManager;
-        private SwitchPreference mFingerprintVib;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,12 +31,6 @@ public class MiscSettings extends SettingsPreferenceFragment
 
         addPreferencesFromResource(R.xml.dnd_misc_tab);
             PreferenceScreen prefSet = getPreferenceScreen();
-
-            // Fingerprint vibration
-            mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
-            mFingerprintVib = (SwitchPreference) prefSet.findPreference("fingerprint_success_vib");
-            if (!mFingerprintManager.isHardwareDetected()){
-                prefSet.removePreference(mFingerprintVib);
 
         mScreenshotType = (ListPreference) findPreference(SCREENSHOT_TYPE);
         int mScreenshotTypeValue = Settings.System.getInt(getActivity().getContentResolver(),
