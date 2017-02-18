8ad1473c8082b8fd5f047bb19196d6989fdd08b9
AKabhishek <abhishekkaushik.star@gmail.com>
2017-02-18 12:50:37 +0000

    Fix build
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- src/com/android/settings/deviceinfo/StorageSettings.java
+++ src/com/android/settings/deviceinfo/StorageSettings.java
@@ -83,6 +83,8 @@ public class StorageSettings extends SettingsPreferenceFragment implements Index
     static int getColorWarning(Resources resources) {
         return resources.getColor(R.color.storage_warning);
     }
+    static final int COLOR_PUBLIC = Color.parseColor("#ff9e9e9e");
+    static final int COLOR_WARNING = Color.parseColor("#fff4511e");
 
     static int[] getColorPrivate(Resources resources) {
         return new int[] {
